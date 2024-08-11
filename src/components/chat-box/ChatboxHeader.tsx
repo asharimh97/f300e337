f832039f7d975f0913e033b0944b05906f83522e
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2024-08-11 21:37:15 +0700

    feat: update chat box header

--- src/components/chat-box/ChatboxHeader.tsx
+++ src/components/chat-box/ChatboxHeader.tsx
@@ -1,31 +1,15 @@
-interface ChatboxHeaderProps {
-  onClick: () => void;
-}
+import UserContext from "@/src/contexts/UserContext";
+import { useContext } from "react";
+import { FaCircleUser } from "react-icons/fa6";
 
-function ChatboxHeader({ onClick }: ChatboxHeaderProps) {
+function ChatboxHeader() {
+  const userName = useContext(UserContext);
   return (
     <div className="p-4 border-b bg-blue-500 text-white rounded-t-lg flex justify-between items-center">
-      <p className="text-lg font-semibold">Admin Bot</p>
-      <button
-        id="close-chat"
-        className="text-gray-300 hover:text-gray-400 focus:outline-none focus:text-gray-400"
-        onClick={onClick}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          ></path>
-        </svg>
-      </button>
+      <div className="flex items-center">
+        <FaCircleUser />
+        <p className="ml-2 text-lg font-semibold">{userName}</p>
+      </div>
     </div>
   );
 }
